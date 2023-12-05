2a13e883fb9ded2f5c22c372d5a0b22f61e024b4
olufikayoolowoyo <olufikayoolowoyo@gmail.com>
2023-12-04 21:14:20 -0500

    CleanUp

--- src/routes/menRoute.js
+++ src/routes/menRoute.js
@@ -1,6 +1,3 @@
-import users from "../data/users";
-const uuid = require("uuid");
-const token = require('jsonwebtoken');
 const mongoose = require("mongoose");
 
 import { addUser,getUsers,findUserById ,removeUser,updateUser} from "../controllers/menController";
@@ -35,26 +32,3 @@ const routes = (app) => {
 
 export default routes;
 
-/*(req, res) => {
-      const id = req.params.id;
-      let _user = users.filter((user) => user.id === parseInt(id));
-
-      if (_user.length > 0) {
-        res.json(_user);
-      } else {
-        res.sendStatus(400);
-      }
-    }
-    (req, res) => {
-      const id = req.params.id;
-      let _user = users.filter((user) => user.id === parseInt(id));
-
-      if (_user.length > 0) {
-        users = users.filter((user) => user.id != parseInt(id));
-        res.json({ msg: "User deleted", users });
-      } else {
-        res.sendStatus(400);
-      }
-    }
-    
-    */
