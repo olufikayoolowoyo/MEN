f2f1b31a2fe9c258a0910f5e68cf50c97413b90b
olufikayoolowoyo <olufikayoolowoyo@gmail.com>
2023-12-04 23:27:52 -0500

    Serving Static files

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const app = express();
 const PORT = 3000;
 
 
+
 import routes  from './src/routes/menRoute';
 routes(app);
+
+//Serve Static files from this folder
+app.use(express.static('assets'));
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
